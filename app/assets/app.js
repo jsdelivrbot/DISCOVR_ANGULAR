b62cac046307053c086307c585b30e8a8324ca6d
weed <ing.molinanestor@gmail.com>
2016-08-24 01:45:50 -0600

    Re-organizacion de la estructura de folder, inclusion de paquete de idiomas, correcciones en la jerarquia de algunos html
    Se a creado una nueva jerarquia de folder, por ende se modificaron las
    referencias de las hojas de estilos, script y archivos multimedias.
    Se agrego angular-translete, en el index, se implemento a modo de prueba
    el multi idioma en ingles y español.
    **Queda pendiente de mejorar y reimplementar en el home.

---
+++ app/assets/app.js
@@ -0,0 +1,27 @@
+var discovrApp = angular.module('DiscovrIndex', ['ngRoute','pascalprecht.translate'])
+    .config(function($translateProvider, $routeProvider) {
+        // Configuración de los idiomas
+        $translateProvider.useStaticFilesLoader({
+            prefix: 'assets/js/languages/',
+            suffix: '.json'
+        });
+        $translateProvider.useLocalStorage();
+        $translateProvider.preferredLanguage('es/es');
+        // Configuración de las rutas
+        $routeProvider
+            .when('/', {
+                templateUrl : '../assets/templates/loguin.html',
+            })
+        /*  .when('/recuperar', {
+                templateUrl : 'templates/recuperar_contrasenia.html'            
+            })
+        */
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
+    .controller('MainController', function($scope, $translate){
+       $scope.changeLang = function changeLangFn(langKey) {
+            $translate.use(langKey);
+       };
+    });
