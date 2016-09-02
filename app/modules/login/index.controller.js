9a0ce0e8682f45b46f0accb55171b2c3c2267283
weed <ing.molinanestor@gmail.com>
2016-09-01 20:55:32 -0600

    implementacion de cambio de idioma

--- app/modules/login/index.controller.js
+++ app/modules/login/index.controller.js
@@ -1,9 +1,38 @@
-discovrApp.controller('Home.IndexController', function($location, AuthenticationService) {
+discovrApp.config(function($translateProvider){
+    // Configuración de los idiomas
+    $translateProvider.useStaticFilesLoader({
+        prefix: 'modules/login/languages/',
+        suffix: '.json'
+    });
+    $translateProvider.useLocalStorage();
+    $translateProvider.preferredLanguage('es');
+    // here the html tag works
+    $translateProvider.useSanitizeValueStrategy('sanitizeParameters');
+});
+discovrApp.controller('Login.IndexController', function($location,AuthenticationService,$scope, $translate){    
     var vm = this;
- 
+
+    vm.login = login;
+
     initController();
 
-    function initController() {
-
-    }
-    });
+    function initController(){
+        //reset login status
+        AuthenticationService.Logout();
+    };
+    function login(){
+        vm.loading = true;
+        AuthenticationService.Login(vm.username, vm.password, function(result){
+            if(result === true){
+                $location.path('/');                    
+            }else{
+                vm.error = 'username or password is incorrect';
+                vm.loading = false;
+            }
+        });
+    };
+    $scope.listLan = ['es', 'en'];
+    $scope.changeLang = function changeLangFn() {
+        $translate.use($scope.listLan);
+    };
+});    
