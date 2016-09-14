9c4102409b0a0da2703395cf66710b7e118e3fdc
weed <ing.molinanestor@gmail.com>
2016-09-13 22:22:59 -0600

    home controller corrections

--- app/modules/home/index.controller.js
+++ app/modules/home/index.controller.js
@@ -20,8 +20,6 @@ discovrApp.controller('Home.IndexController', function($localStorage,$location,A
         console.log(opt);
         $translate.use('home/languages/' + opt); 
     };
-<<<<<<< HEAD
-
     $scope.dataArray = [
       {
         src: 'http://conlospiesporlatierra.com/wp-content/uploads/2013/07/img_9394.jpg'
@@ -36,35 +34,10 @@ discovrApp.controller('Home.IndexController', function($localStorage,$location,A
         src: 'http://img.ev.mu/images/villes/40755/1605x642/40755.jpg'
       },
       {
-        src: 'http://endimages.s3.amazonaws.com/legacy/1355009715_RUTA%20CA%C3%91ON%20DE%20SOMOTO%20091212.jpg'
-=======
-     $scope.dataArray = [
-      {
-        src: 'http://trunkweed.com/uploads/posts/images/599694-panoramic-nature-wallpaper-1.jpg'
-      },
-      {
-        src: 'http://trunkweed.com/uploads/posts/images/590341-blue-panoramic-nature-background.jpg'
-      },
-      {
-        src: 'http://cdn.wallpapersafari.com/34/30/oDlPQr.jpg'
-      },
-      {
-        src: 'http://trunkweed.com/uploads/posts/images/599708-panoramic-nature-wallpaper.jpg'
-      },
-      {
-        src: 'http://trunkweed.com/uploads/posts/images/590341-blue-panoramic-nature-background.jpg'
->>>>>>> refs/remotes/origin/master
-      },
-      {
         src: 'http://trunkweed.com/uploads/posts/images/590341-blue-panoramic-nature-background.jpg'
       },
       {
         src: 'http://trunkweed.com/uploads/posts/images/590341-blue-panoramic-nature-background.jpg'
       }
     ];
-<<<<<<< HEAD
-
 });
-=======
-});
->>>>>>> refs/remotes/origin/master
