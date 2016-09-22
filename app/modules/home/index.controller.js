71aa65960dc6815c60e5fc74e9413c9792133d11
Alex Flores <alexjf14@gmail.com>
2016-09-22 11:51:34 -0600

    modificasiones y flexbox

--- app/modules/home/index.controller.js
+++ app/modules/home/index.controller.js
@@ -25,27 +25,77 @@ discovrApp.controller('Home.IndexController', function($localStorage,$location,A
     $scope.noWrapSlides = false;
     $scope.active = 0;
     var slides = $scope.slides = [
-        {image: 'http://www.pixolo.it/wp-content/uploads/2012/07/wallpaper-1867190.jpg',
+        {image: '../../assets/files/img/main/prinsipalSlider/1.jpg',
       id: 0},
-
-      {image: 'http://www.que-come.com/wp-content/uploads/2015/08/que-comen-las-ranas.jpg',
+      {image: '../../assets/files/img/main/prinsipalSlider/2.jpg',
       id: 1},
-
-        {image: 'http://www.pixolo.it/wp-content/uploads/2012/07/wallpaper-1667348.jpg',
-      id: 2}
+      {image: '../../assets/files/img/main/prinsipalSlider/3.jpg',
+      id: 2},
+      {image: '../../assets/files/img/main/prinsipalSlider/4.jpg',
+      id: 3},
+      {image: '../../assets/files/img/main/prinsipalSlider/5.jpg',
+      id: 4},
+       {image: '../../assets/files/img/main/prinsipalSlider/6.jpg',
+      id: 5}
     ];
     var currIndex = 0;
-
+/*------------------------------------------------------------------------------------------------*/
     $scope.myInterval1 = 4000;
     $scope.noWrapSlides1 = false;
     $scope.active1 = 0;
     var slides1 = $scope.slides1 = [
-        {image: 'http://www.pixolo.it/wp-content/uploads/2012/07/wallpaper-1867190.jpg',
+        {image: '../../assets/files/img/main/eventos/1.jpg',
       id: 0},
-        {image: 'http://www.pixolo.it/wp-content/uploads/2012/07/wallpaper-1667348.jpg',
-      id: 1}
+      {image: '../../assets/files/img/main/eventos/2.jpg',
+      id: 1},
+      {image: '../../assets/files/img/main/eventos/3.jpg',
+      id: 2}
+    ];
+    var currIndex = 0;
+/*-------------------------------------------------------------------------------------------------*/
+     $scope.myInterval2 = 3000;
+    $scope.noWrapSlides2 = false;
+    $scope.active2 = 0;
+    var slides2 = $scope.slides2 = [
+        {image: '../../assets/files/img/main/dondeIr/1.jpg',
+      id: 0},
+      {image: '../../assets/files/img/main/dondeIr/2.jpg',
+      id: 1},
+      {image: '../../assets/files/img/main/dondeIr/3.jpg',
+      id: 2}
     ];
     var currIndex = 0;
 
+/*-------------------------------------------------------------------------------------------------*/
+     $scope.myInterval3 = 4500;
+    $scope.noWrapSlides3 = false;
+    $scope.active3 = 0;
+    var slides3 = $scope.slides3 = [
+      {image: '../../assets/files/img/main/galeria/1.jpg',
+      id: 0},
+      {image: '../../assets/files/img/main/galeria/2.jpg',
+      id: 1},
+      {image: '../../assets/files/img/main/galeria/3.jpg',
+      id: 2},
+       {image: '../../assets/files/img/main/galeria/4.jpg',
+      id: 3}
+    ];
+    var currIndex = 0;
+
+    /*-------------------------------------------------------------------------------------------------*/
+    $scope.myInterval4 = 3500;
+    $scope.noWrapSlides4 = false;
+    $scope.active4 = 0;
+    var slides4 = $scope.slides4 = [
+      {image: '../../assets/files/img/main/alojamiento/1.jpg',
+      id: 0},
+      {image: '../../assets/files/img/main/alojamiento/2.jpg',
+      id: 1},
+      {image: '../../assets/files/img/main/alojamiento/3.jpg',
+      id: 2},
+       {image: '../../assets/files/img/main/alojamiento/4.jpg',
+      id: 3}
+    ];
+    var currIndex = 0;
    
 });
