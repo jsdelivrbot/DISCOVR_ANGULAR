ad0b15f7303d41799339e13677ab1626f89f6753
Weed <ing.molinanestor@gmail.com>
2016-10-12 16:22:20 -0600

    / ‘app/services/authentication.service.js’

--- app/services/authentication.service.js
+++ app/services/authentication.service.js
@@ -51,7 +51,9 @@ discovrApp.factory('AuthenticationService', function (
           var token = jwtHelper.decodeToken(response.token);
           //store username and token in local storage to keep user logged in between paga refreshes
           $localStorage.currentUser = {id: token.user_id, username: username, token: response.token };
-          
+          CreateClient(name,surname,birthday,genre,city).then(function(client){
+            console.log();
+          };
           console.log(client);
           CreateTourist(token.user_id, client.id);
           //add jwt token to auth header for all requests made by the $http services
