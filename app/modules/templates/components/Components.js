09fe1cc20716e9242026b3b34f3d2dd6182ab86e
weed <ing.molinanestor@gmail.com>
2016-12-07 18:45:59 -0600

    Mejorasen Tab de la vista housing View

--- app/modules/templates/components/Components.js
+++ app/modules/templates/components/Components.js
@@ -59,6 +59,38 @@ discovrApp.component('floatTile', {
         };
       }
   });
+/*-------------------------    Navbar    --------------------------------*/
+discovrApp.component('navBar', {
+    templateUrl:'modules/templates/components/Navbar.html',
+    bindings: {
+            info: '<',
+            modal: '<',
+            title: '<',
+            option: '<'
+        },
+    controller: function($uibModal) {
+        var $ctrl = this;
+        
+        $ctrl.open = function() {
+            $uibModal.open({                
+                template: '<modal-'+$ctrl.modal+' modal-data="$ctrl.modalData" $close="$close(result)" $dismiss="$dismiss(reason)"></modal-'+$ctrl.modal+'>',
+                controller: ['modalData', function(modalData) {
+                    var $ctrl = this;
+                    $ctrl.modalData = modalData;
+                }],
+                controllerAs: '$ctrl',
+                resolve: {
+                    modalData: $ctrl.info
+                }
+            }).result.then(function(result) {
+                console.info("I was closed, so do what I need to do myContent's controller now and result was->");
+                console.info(result);
+            }, function(reason) {
+                console.info("I was dimissed, so do what I need to do myContent's controller now and reason was->"+reason);
+            });
+        };
+      }
+  });  
 /*-------------------------    ModalPreview    --------------------------------*/
 discovrApp.component('modalPreview', {
     templateUrl:'modules/templates/components/ModalPreview.html',
@@ -115,6 +147,34 @@ discovrApp.component('modalMap', {
 /*-------------------------    ModalBook    --------------------------------*/
 discovrApp.component('modalBook', {
     templateUrl:'modules/templates/components/ModalBook.html',
+    bindings: {
+            $close: '&',
+            $dismiss: '&',
+            modalData: '<'
+        },
+        controller: function($sce) {
+            var $ctrl = this;
+
+            $ctrl.trustSrc = function(src) {
+                return $sce.trustAsResourceUrl(src);
+            };
+            $ctrl.handleClose = function() {
+                console.info("in handle close");
+                $ctrl.$close({
+                    result: $ctrl.modalData
+                });
+            };
+            $ctrl.handleDismiss = function() {
+                console.info("in handle dismiss");
+                $ctrl.$dismiss({
+                    reason: 'cancel'
+                });
+            };
+        }  
+  });
+  /*-------------------------    ModalItinerary    --------------------------------*/
+discovrApp.component('modalItinerary', {
+    templateUrl:'modules/templates/components/ModalItinerary.html',
     bindings: {
             $close: '&',
             $dismiss: '&',
